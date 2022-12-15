5ccb6ff4ebe01d95bdac559b071ff252fd12388b
Fuat Can Akgün <fuatcanakgun@fuatmac.local>
2022-12-15 23:44:34 +0300

    svelte basics integrated and tested.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="/" on:click|preventDefault={() => {}}>About</a></li>
+        <li><a href="/" on:click|preventDefault={() => {}}>Projects</a></li>
+        <li><a href="/" on:click|preventDefault={() => {}}>Contact</a></li>
+    </ul>
+</nav>
+
+<slot />
